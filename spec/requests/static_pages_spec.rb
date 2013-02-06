87c513f4720647cc9bf09c3c0c69308e248da061
assqwert <zh_tam@hotmail.com>
2013-02-05 23:52:25 -0800

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,8 +11,13 @@ describe "Static Pages" do
 		
 		it "it should have title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title',:text => 'Home')
+      page.should have_selector('title',:text => 'Kahang Organic')
     end
+		
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title',:text => 'Home')
+		end
 
   end
 	
