f3389e8baec0dc5ea5720f5623d6c116bd8d4fca
assqwert <zh_tam@hotmail.com>
2013-02-05 19:44:41 -0800

    :q

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,24 +4,40 @@ describe "Static Pages" do
 
   describe "Home page" do
 
-    it "it should have the content 'Sample App'" do
+    it "it should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1',:text => 'Sample App')
     end
-  end
+		
+		it "it should have title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text => 'Home')
+    end
 
+  end
+	
   describe "Help Page" do
-     it "it should have the content 'Help'" do
+     it "it should have the h1 'Help'" do
        visit '/static_pages/help'
-       page.should have_content('Help')
+       page.should have_selector('h1',:text => 'Help')
     end
+		it "it should have the title 'Help'" do
+       visit '/static_pages/help'
+       page.should have_selector('title',:text=>'Help')
+    end
+
   end
   
   describe "About Page" do
-     it "it should have the content 'About us'" do
+     it "it should have h1 'About us'" do
        visit '/static_pages/about'
-       page.should have_content('About us')
+       page.should have_selector('h1',:text => 'About us')
     end
+		it "it should have title 'About us'" do
+       visit '/static_pages/about'
+       page.should have_selector('title',:text => 'About us')
+    end
+
   end
 
 end
